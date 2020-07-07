d55293558a6b842d40ca53a41d1d22c1f44d9e22
VinoxM <31297195+VinoxM@users.noreply.github.com>
2020-07-08 07:11:46 +0800

    1123

--- src/client/ybplugins/yobot_msg_extra.py
+++ src/client/ybplugins/yobot_msg_extra.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import random
 from typing import Any, Dict, Union
 
 from aiocqhttp.api import Api
@@ -18,10 +19,18 @@ class Message_Extra:
         self.setting = glo_setting
         # 加载bot的api
         self.api = bot_api
-        # 存储上一条信息和上上条信息
-        self.last_msg = self.pre_last_msg = None
+        # 存储上一条信息
+        self.last_msg = None
+        # 存储消息重复次数
+        self.equal_count = 1
         # 存储上一条复读信息，以免重复复读
         self.last_repeat = None
+        self.repeat_count = self.setting.get("repeat_count",2)
+        self.repeat_random = self.setting.get("repeat_random",False)
+        if self.repeat_random:
+            self.repeat_range = self.setting.get("repeat_range",[2,3])
+            self.repeat_random_count = random.randint(self.repeat_range[0],self.repeat_range[1])
+            print("随机数值为",self.repeat_random_count)
         self.pre_path = "D:\\Documents"
 
     async def send_img(self,group_id,path):
@@ -41,20 +50,47 @@ class Message_Extra:
         # 仅处理群聊消息
         if ctx['message_type'] == 'group':
             # 复读
-            # 判断上条和上上条信息是否已有值
-            if ctx['raw_message'] == ctx['message']:
-                if not self.last_msg is None and not self.pre_last_msg is None:
-                    # 判断上条信息和上上条信息相同
-                    if self.last_msg == self.pre_last_msg:
-                        msg = ctx['message']
-                        # 判断发送的信息和前两条信息相同
-                        if self.last_msg == msg and not self.last_repeat == msg:
-                            # 开始复读，发送复读信息
-                            await self.api.send_group_msg(group_id=ctx["group_id"], message=msg)
-                            # 记录本次复读信息
-                            self.last_repeat = msg
-                self.last_msg = ctx['message']
-                self.pre_last_msg = self.last_msg
+            # 复读开关
+            if self.setting.get("repeat_on",False):
+                # 上条信息判断不为空
+                if not self.last_msg is None:
+                    # 判断是否为过滤信息、是否与上条信息相同、复读信息是否重复
+                    if ctx['raw_message']==ctx['message'] and self.last_msg==ctx['message'] and self.last_repeat!=ctx['message']:
+                        # 重复信息数加一
+                        self.equal_count+=1
+                        # 是随机复读值
+                        if self.repeat_random:
+                            # 达到复读值
+                            if self.repeat_random_count==self.equal_count:
+                                # 开始复读
+                                await self.api.send_group_msg(group_id=ctx['group_id'],message=ctx['message'])
+                                # 重置数据
+                                self.equal_count=1
+                                self.last_repeat=ctx['message']
+                                # 重新随机随机数
+                                self.repeat_random_count=random.randint(self.repeat_range[0],self.repeat_range[1])
+                                print("随机数值为",self.repeat_random_count)
+                        # 不是随机复读值
+                        else:
+                            # 达到复读值    
+                            if self.repeat_count==self.equal_count:
+                                # 开始复读
+                                await self.api.send_group_msg(group_id=ctx['group_id'],message=ctx['message'])
+                                # 重置数据
+                                self.equal_count=1
+                                self.last_repeat=ctx['message']
+                    # 当前信息不符合复读要求
+                    else:
+                        # 记录上一条数据
+                        self.last_repeat=ctx['message']
+                        # 重置数据
+                        self.equal_count=1
+                        self.last_repeat=None
+                # 上条信息为空
+                else:
+                    # 存储当前信息为上条信息
+                    self.last_msg=ctx['message']
+
 
             # curse
             if ctx['raw_message'] != ctx['message']:
