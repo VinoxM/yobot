8161bab6636bc3e55833274c6fa560cc1418f53c
VinoxM <31297195+VinoxM@users.noreply.github.com>
2020-07-21 17:40:26 +0800

    123

--- src/client/ybplugins/gacha.py
+++ src/client/ybplugins/gacha.py
@@ -349,11 +349,10 @@ class Gacha:
             if not os.path.exists(gacha_path):
                 os.makedirs(gacha_path)
             gacha_file = os.path.join(gacha_path, filename)
-            gacha_star = Image.open(os.path.join(self.resource_path, "gacha", "unit", "star.png")).resize((16, 16),Image.ANTIALIAS)
-            gacha_img = Image.new('RGB', (128, 128))
-            gacha_img.paste(Image.open(localfile), (0,0))
+            gacha_star = Image.open(os.path.join(self.resource_path, "gacha", "unit", "star.png")).resize((16, 16), Image.ANTIALIAS).convert('RGBA')
+            gacha_img = Image.open(localfile).convert('RGBA')
             for i in range(1, r[1]+1):
-                gacha_img.paste(gacha_star, (4+14*i,-16))
+                gacha_img.paste(gacha_star, (4+14*i, 112))
             gacha_img.save(gacha_file)
             local_files.append(gacha_file)
         img_col = 5
