11a86f0d04c838837662b541e8cef2fabee9525d
VinoxM <31297195+VinoxM@users.noreply.github.com>
2020-07-22 11:20:43 +0800

    123

--- src/client/ybplugins/gacha.py
+++ src/client/ybplugins/gacha.py
@@ -109,6 +109,7 @@ class Gacha:
                     self.pool_up[k]["up"] += "★★：{}，".format(",".join(self.pool_up[k]["★★"]))
                 if len(self.pool_up[k]["★"]) > 0:
                     self.pool_up[k]["up"] += "★：{}，".format(",".join(self.pool_up[k]["★"]))
+                self.pool_up[k]["up"] = self.pool_up[k]["up"][:-1]
 
 
     async def update_nicknames(self, flag: bool = False) -> str:
