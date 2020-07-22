c64dbac289f80132bbb315da94d1c40721c0c667
VinoxM <31297195+VinoxM@users.noreply.github.com>
2020-07-22 10:40:11 +0800

    123

--- src/client/ybplugins/gacha.py
+++ src/client/ybplugins/gacha.py
@@ -355,7 +355,7 @@ class Gacha:
         return reply
 
     async def handle_result(self,result: List):
-        print(result)
+        # print(result)
         local_files = []
         for r in result:
             char_id = self.nickname_dict[str(r[0])][0]
